1bd6aa90e6388bb2a0281cee1a889ba5c6b1d329
umxr <umarg1997@gmail.com>
2020-11-12 17:50:11 +0000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Umar Gora on 12/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
